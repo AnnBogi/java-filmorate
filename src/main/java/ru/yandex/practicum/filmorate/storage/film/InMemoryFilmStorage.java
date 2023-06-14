2e4c91a7ce687f758bbfda156e41bcce42612cb1
Bogomolova <BatAnn04@yandex.ru>
2023-06-15 01:52:45 +0300

    Проверка 1.00 - ТЗ №10

--- src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
+++ src/main/java/ru/yandex/practicum/filmorate/storage/film/InMemoryFilmStorage.java
@@ -62,9 +62,11 @@ public class InMemoryFilmStorage implements FilmStorage {
     private void filmValidation(Film film) throws ValidationException {
         if (film.getReleaseDate().isBefore(LocalDate.parse("1895-12-20"))
                 || film.getReleaseDate().isAfter(LocalDate.now())) {
-            throw new ValidationException("Некорректно указана дата релиза."); }
+            throw new ValidationException("Некорректно указана дата релиза.");
+        }
         if (film.getName().isEmpty()) {
-            throw new ValidationException("Некорректно указано название фильма."); }
+            throw new ValidationException("Некорректно указано название фильма.");
+        }
         if (film.getDescription().length() > 200) {
             throw new ValidationException("Превышено количество символов в описании фильма.");
         }
